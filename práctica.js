ef13f57ab58a3f5ed0a61725c0e2216328cf2880
AgustinaCorrea1 <agustinacorrea1607@gmail.com>
2023-03-02 14:02:11 -0300

    json

---
+++ práctica.js
@@ -0,0 +1,15 @@
+var Login = { "email":"agustinacorrea1607@gmail.com", "contraseña":2001 };
+var RedSocial = "Instagram";
+var Programa = "Argentina Programa";
+var Experiencia = "Niñera";
+var Educación = "Escuela Secundaria, Profesorado de Biología y Argentina Programa";
+var HardSkills = "HTML, CSS y Bootstrap";
+var SoftSkills = "Adaptación a los cambios, comunicación y gestión del tiempo";
+var Idiomas = "Español, ingles e italiano";
+var Proyectos = "Portfolio web";
+
+const Portfolio = {
+    nombre: "Agustina Correa",
+    edad: 21
+};
+console.log(JSON.stringify(Portfolio));
